6cbbbd2f2693a3f2fb6cbfe767b754d22c05036c
Philip Falkner <philip.falkner@gmail.com>
2019-06-23 20:25:33 -0400

    Add spinning wheel to screen
    - Currently just eye candy
    - The idea is that later the wheel will spin when you press the button,
      and land on the chosen lunch

--- src/App.vue
+++ src/App.vue
@@ -12,29 +12,64 @@
     </b-navbar>
     <br />
 
-    <div v-if='selectedLunch'>
-      <b-card :title='selectedLunch.label' :img-src='selectedLunch.img' style='max-width: 30rem; margin: auto;'>
-        <b-card-text>{{selectedLunch.info}}</b-card-text>
-      </b-card>
-    </div>
-    <div v-else style='text-align: center; color: rgb(172, 172, 172)'>
-      <p>You haven't picked a lunch yet...</p>
-    </div>
+    <b-container>
+      <b-row>
+        <b-col>
+          <SpinWheel :segments='lunchOptions' class='lunchWheel' />
+        </b-col>
+        <b-col>
+          <div v-if='selectedLunch'>
+            <b-card :title='selectedLunch.label' :img-src='selectedLunch.img' style='max-width: 30rem; margin: auto;'>
+              <b-card-text>{{selectedLunch.info}}</b-card-text>
+            </b-card>
+          </div>
+          <div v-else style='text-align: center; color: rgb(172, 172, 172)'>
+            <p>You haven't picked a lunch yet...</p>
+          </div>
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script>
+import SpinWheel from './components/SpinWheel.vue'
+
 const lunchOptions = [
-  { label: 'Fries', info: 'Great choice! Fries rule!',        img: 'https://cms.splendidtable.org/sites/default/files/styles/w2000/public/french-fries.jpg?itok=FS-YwUYH' },
-  { label: 'Sushi', info: 'A veggie role for me please!',     img: 'https://www.parcelpal.com/wp-content/uploads/2017/06/MomoSushi.jpg' },
-  { label: 'Salad', info: 'A very healthy choice.',           img: 'https://www.ndtv.com/cooks/images/tossed-mixed-salad-620.jpg' },
-  { label: 'Pizza', info: 'Delecious, yummy, tasty pizza...', img: 'https://recipes.timesofindia.com/photo/53110049.cms' }
+  {
+    label: "Fries",
+    colour: "#fff200",
+    info: "Great choice! Fries rule!",
+    img: "https://cms.splendidtable.org/sites/default/files/styles/w2000/public/french-fries.jpg?itok=FS-YwUYH"
+  },
+  {
+    label: "Sushi",
+    colour: "#f6989d",
+    info: "A veggie role for me please!",
+    img: "https://www.parcelpal.com/wp-content/uploads/2017/06/MomoSushi.jpg"
+  },
+  {
+    label: "Salad",
+    colour: "#3cb878",
+    info: "A very healthy choice.",
+    img: "https://www.ndtv.com/cooks/images/tossed-mixed-salad-620.jpg"
+  },
+  {
+    label: "Pizza",
+    colour: "#ee1c24",
+    info: "Delecious, yummy, tasty pizza...",
+    img: "https://recipes.timesofindia.com/photo/53110049.cms"
+  }
 ]
 
 export default {
   name: 'lunchify-app',
+  components: {
+    SpinWheel
+  },
   data: function () {
     return {
+      lunchOptions: lunchOptions,
       selectedLunch: null
     }
   },
@@ -45,3 +80,10 @@ export default {
   }
 }
 </script>
+
+<style>
+.lunchWheel {
+  width: 30rem;
+  height: 30rem;
+}
+</style>
